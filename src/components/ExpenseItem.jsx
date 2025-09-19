d482d66ebcc2d4e3b897c36bf4d3d8f97a184163
Ajay Sahani <sajay3159@gmail.com>
2025-09-19 15:32:00 +0530

    Creating Reusable Components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -7,7 +7,8 @@ function ExpenseItem(props) {
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.price}</div>
-                <div className="expense-item__location">{props.location}</div>
+            </div>
+            <div className="expense-item__location">{props.location}
             </div>
         </div>
     );
