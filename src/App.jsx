f052f74e3c33a95a3893639b0e5c9569ed7299ae
Ajay Sahani <sajay3159@gmail.com>
2025-09-19 15:12:56 +0530

    Props Object & Passing Data to Child Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-      <ExpenseItem />
-    </>
-  )
+    <div>
+      <h1>Let's get Started</h1>
+      <ExpenseItem
+        date={new Date(2023, 7, 15)}
+        title="Insurance"
+        price="50"
+        location="Bangalore"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 3, 25)}
+        title="Book"
+        price="20"
+        location="Delhi"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 10, 11)}
+        title="Pen"
+        price="5"
+        location="Hyerabad"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 1, 14)}
+        title="Laptop"
+        price="200"
+        location="Mumbai"
+      ></ExpenseItem>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,18 +1,14 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 7, 15).toISOString();
-    const expenseTitle = "Insurance";
-    const expenseAmount = 50;
-
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{expenseDate}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.price}</div>
+                <div className="expense-item__location">{props.location}</div>
             </div>
-            <div className="expense-item__location">Bangalore</div>
         </div>
     );
 }
