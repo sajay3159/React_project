4e9e905036b51ca8092ef7368e09fcdaae553d78
Ajay Sahani <sajay3159@gmail.com>
2025-09-20 10:00:37 +0530

    Listening for Events

--- src/components/Expense/ExpenseItem.jsx
+++ src/components/Expense/ExpenseItem.jsx
@@ -4,6 +4,9 @@ import "./ExpenseItem.css";
 import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
+    function handleClickHandler(event) {
+        console.log("Button clicked");
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}> </ExpenseDate>
@@ -11,6 +14,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.price}</div>
             </div>
+            <button onClick={handleClickHandler}>Change Title</button>
         </Card>
     );
 }
