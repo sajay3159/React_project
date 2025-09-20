24128ddb73dcf1f4e225e4000eadec4bcd83d57c
Ajay Sahani <sajay3159@gmail.com>
2025-09-20 10:03:08 +0530

    Introduction to React Hooks

--- src/components/Expense/ExpenseItem.jsx
+++ src/components/Expense/ExpenseItem.jsx
@@ -2,19 +2,22 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from '../UI/Card'
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
-    function handleClickHandler(event) {
-        console.log("Button clicked");
+    const [title, setTitle] = useState(props.title);
+
+    function buttonClickHandler() {
+        setTitle("New Title");
     }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}> </ExpenseDate>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.price}</div>
             </div>
-            <button onClick={handleClickHandler}>Change Title</button>
+            <button onClick={buttonClickHandler}>Change Title</button>
         </Card>
     );
 }
