ed5fc7692fc62c961a53b02ffd1becb849ac98cf
Ajay Sahani <sajay3159@gmail.com>
2025-09-21 11:25:06 +0530

    Rendering Expenses

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import Expenses from "./components/Expense/Expenses";
 import './App.css';
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
 
 const App = () => {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
     { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
     { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
     { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
-  ];
+  ]);
 
   const addExpenseHandler = (expense) => {
-    console.log(expense)
-  }
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+  };
 
   return (
     <div>
