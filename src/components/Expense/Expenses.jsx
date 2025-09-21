bad3f50f8ba51f3550ee91b288ea2545c683018b
Ajay Sahani <sajay3159@gmail.com>
2025-09-21 19:12:09 +0530

    Conditional Rendering

--- src/components/Expense/Expenses.jsx
+++ src/components/Expense/Expenses.jsx
@@ -1,10 +1,9 @@
-// Write your code at relevant places in the code below:
 
 import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState("2023");
@@ -12,26 +11,35 @@ const Expenses = (props) => {
     const changeFilterHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
-    const expensesItem = props.expenses.filter((expense) => {
+
+    const filteredExpenses = props.expenses.filter((expense) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
-    console.log(expensesItem)
+
+    let expensesContent = <p>No expenses found</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => {
+            return (
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    date={expense.date}
+                    price={expense.price}
+                />
+            );
+        });
+    }
+    let expensesOneContent = filteredExpenses.length === 1 && <p>Only one expense found.Please add more</p>;
+
     return (
         <Card className="expenses">
             <ExpensesFilter
                 selected={filteredYear}
                 onChangeFilter={changeFilterHandler}
             />
-            {expensesItem.map((expense) => {
-                return (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        date={expense.date}
-                        price={expense.price}
-                    />
-                );
-            })}
+            {expensesContent}
+            {expensesOneContent}
         </Card>
     );
 };
